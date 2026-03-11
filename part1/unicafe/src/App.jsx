2a143ce0aa88faca60c427d5e988e870e9b6cd21
puiusabin <puiumihaisabin@gmail.com>
2026-03-11 15:39:44 +0200

    add tbody to table to fix console warning for 1.11*

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,30 +11,32 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <table>
-        <tr>
-          <td>good</td>
-          <td>{good}</td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td>{neutral}</td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td>{bad}</td>
-        </tr>
-        <tr>
-          <td>all</td>
-          <td>{good + neutral + bad}</td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td>{(good - bad) / (good + neutral + bad)}</td>
-        </tr>
-        <tr>
-          <td>positive</td>
-          <td>{(good / (good + neutral + bad)) * 100}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{good + neutral + bad}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{(good - bad) / (good + neutral + bad)}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{(good / (good + neutral + bad)) * 100}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
