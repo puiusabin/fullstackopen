1540aac12ff962554623395f42a63e513606309e
puiusabin <puiumihaisabin@gmail.com>
2026-03-18 18:04:40 +0200

    solve 2.19*: Data for countries, step 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,24 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const CountryInfo = ({ country }) => {
+  const languages = Object.values(country.languages);
+  const flag = country.flags.png;
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>capital {country.capital[0]}</div>
+      <div>area {country.area}</div>
+      <h2>Languages</h2>
+      <ul>
+        {languages.map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img src={flag} alt="flag of country" />
+    </div>
+  );
+};
+
+const CountryItem = ({ country }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <li>
+      {country.name.common} <button onClick={() => setShow(true)}>show</button>
+      {show ? <CountryInfo country={country} /> : null}
+    </li>
+  );
+};
+
 const Countries = ({ filteredCountries }) => {
   if (filteredCountries.length === 1) {
     const country = filteredCountries[0];
-    const languages = Object.values(country.languages);
-    const flag = country.flags.png;
     return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <div>capital {country.capital[0]}</div>
-        <div>area {country.area}</div>
-        <h2>Languages</h2>
-        <ul>
-          {languages.map((lang) => (
-            <li key={lang}>{lang}</li>
-          ))}
-        </ul>
-        <img src={flag} alt="flag of country" />
-      </div>
+      <>
+        <CountryInfo country={country} />
+      </>
     );
   } else if (filteredCountries.length >= 10) {
     return <div>too many matches, specify another filter</div>;
@@ -26,7 +44,7 @@ const Countries = ({ filteredCountries }) => {
   return (
     <div>
       {filteredCountries.map((country) => (
-        <li key={country.name.common}>{country.name.common}</li>
+        <CountryItem key={country.name.common} country={country} />
       ))}
     </div>
   );
