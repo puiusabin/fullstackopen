e95c4e767ad9145b0c375a7c8c1ff299bbffd91b
puiusabin <puiumihaisabin@gmail.com>
2026-03-22 11:42:30 +0200

    update port for 3.10 Phonebook backend step 10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -85,7 +85,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server listening on port ${3001}`);
 });
