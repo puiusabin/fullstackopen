2864e6892ea623c98b9755b2429239f94273993a
puiusabin <puiumihaisabin@gmail.com>
2026-03-22 23:37:49 +0200

    solve 3.12: Command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("please enter password");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ays58hc.mongodb.net/phonebookApp?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 5) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const note = new Person({
+    name: name,
+    number: number,
+  });
+
+  note.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+}
