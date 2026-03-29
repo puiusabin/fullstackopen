81c22f590ebcbf7113a35f26b182e9056c177844
puiusabin <puiumihaisabin@gmail.com>
2026-03-29 16:16:07 +0300

    solve 3.16: Phonebook database, step 4 and fix some issues from previous exercises

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -40,19 +40,26 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Person.findById(id)
     .then((person) => {
-      response.json(person);
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
     })
-    .catch(() => response.status(404).end());
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  Person.findByIdAndDelete(id).exec();
-  response.status(204).end();
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -74,6 +81,16 @@ app.post("/api/persons", (request, response) => {
     }
   });
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error);
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
