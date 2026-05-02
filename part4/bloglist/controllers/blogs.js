cd629ce6249693ec8cdb757af22449b0ff5fed59
puiusabin <puiumihaisabin@gmail.com>
2026-05-02 23:55:16 +0300

    solve 4.17: Blog List Expansion, step 5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
   response.json(blogs);
 });
@@ -11,11 +12,21 @@ blogsRouter.post("/", async (request, response) => {
   if (!request.body.title || !request.body.url) {
     return response.status(400).json({ error: "Bad Request" });
   }
-  const newBlog = { ...request.body, likes: request.body.likes ?? 0 };
+  const users = await User.find({});
+  const user = users[0];
+  const newBlog = {
+    ...request.body,
+    likes: request.body.likes ?? 0,
+    user: users[0].id,
+  };
   const blog = new Blog(newBlog);
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
