07db5d4458b5bc4096954f4f4832b5f787afdb15
puiusabin <puiumihaisabin@gmail.com>
2026-05-04 12:02:52 +0300

    solve 4.20*: Blog List Expansion, step 8

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
@@ -20,6 +21,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,15 +3,6 @@ const jwt = require("jsonwebtoken");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
@@ -22,8 +13,10 @@ blogsRouter.post("/", async (request, response) => {
   if (!request.body.title || !request.body.url) {
     return response.status(400).json({ error: "Bad Request" });
   }
-
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!request.token) {
+    return response.status(401).json({ error: "invalid token" });
+  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid" });
   }
