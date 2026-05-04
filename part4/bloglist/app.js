0b5844dc0181198ab1b1d16a70d3730dac355e30
puiusabin <puiumihaisabin@gmail.com>
2026-05-04 11:41:30 +0300

    solve 4.18: Blog List Expansion, step 6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
@@ -21,5 +22,6 @@ mongoose
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
