b9e7c44db8617f2320c81be8d6bd558bca651fb6
puiusabin <puiumihaisabin@gmail.com>
2026-05-12 21:15:35 +0300

    solve 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders blog title and author, but does not render its URL and number of likes", () => {
@@ -18,4 +19,29 @@ test("renders blog title and author, but does not render its URL and number of l
   expect(titleAuthor).toBeDefined();
   expect(url).toBeNull();
   expect(likes).toBeNull();
+});
+
+test("clicking the button shows url and likes", async () => {
+  const blog = {
+    title: "test blog",
+    author: "Sabin Puiu",
+    url: "https://example.com/test-blog",
+    likes: 3,
+    user: {
+      name: "sabin",
+    },
+  };
+
+  render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const url = screen.getByText(blog.url, { exact: false });
+  const likes = screen.queryByText(`likes ${blog.likes}`, { exact: false });
+
+  expect(url).toBeDefined();
+  expect(likes).toBeDefined();
 });
