a1fd5c6765e9df621b75f52f88a91e19c936b4de
puiusabin <puiumihaisabin@gmail.com>
2026-05-12 22:25:14 +0300

    solve 5.15: Blog List Tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -45,3 +45,31 @@ test("clicking the button shows url and likes", async () => {
   expect(url).toBeDefined();
   expect(likes).toBeDefined();
 });
+
+test("clicking the like button twice calls the event handler twice", async () => {
+  const blog = {
+    title: "test blog",
+    author: "Sabin Puiu",
+    url: "https://example.com/test-blog",
+    likes: 3,
+    user: {
+      name: "sabin",
+    },
+  };
+
+  const mockHandler = vi.fn();
+
+  render(<Blog blog={blog} addLike={mockHandler} />);
+
+  const user = userEvent.setup();
+
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const likeButton = screen.getByText("like");
+
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(mockHandler.mock.calls).toHaveLength(2);
+});
