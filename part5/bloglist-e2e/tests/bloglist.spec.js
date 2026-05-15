f4ad7f72df3f1913a376d40e2179e153eaf3f4c4
puiusabin <puiumihaisabin@gmail.com>
2026-05-15 11:31:32 +0300

    5.17: Blog List End To End Testing, step 1

---
+++ part5/bloglist-e2e/tests/bloglist.spec.js
@@ -0,0 +1,12 @@
+const { test, expect, beforeEach, describe } = require("@playwright/test");
+
+describe("Blog app", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+
+  test("Login form is shown", async ({ page }) => {
+    await expect(page.getByText("username")).toBeVisible();
+    await expect(page.getByText("password")).toBeVisible();
+  });
+});
